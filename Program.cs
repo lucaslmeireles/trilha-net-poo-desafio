4cab9b142fa8063bbe5d3db494ec36cf13aa2456
lucaslmeireles <86924068+lucaslmeireles@users.noreply.github.com>
2023-12-27 20:03:44 +0000

    tes

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
 Nokia n = new Nokia("2487556", "Windows Phone", "08800144552", 8);
-Iphone i = new Iphone("2487556", "Windows Phone", "08800144552", 8);
+Iphone i = new Iphone("5487746", "Iphone 14", "1222154587", 64);
 
 n.InstalarAplicativo("Uber");
 i.InstalarAplicativo("Uber");
